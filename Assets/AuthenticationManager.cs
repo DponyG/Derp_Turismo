2951617484644c6a2fcfb258da25f2d69bc304c8
Darkpony <dponyg@gmail.com>
2018-11-21 21:42:14 -0800

    Files were to big pushing again

---
+++ Assets/AuthenticationManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AuthenticationManager : MonoBehaviour {
+
+    public GameObject buttonJoinGame;
+    public GameObject buttonRegister;
+    public GameObject mainMenu;
+
+    public GameObject fieldUserName;
+    public GameObject fieldEmailAddress;
+    public GameObject fieldPassword;
+    public GameObject fieldReenterPassword;
+
+    public Text userName;
+    public Text textEmail;
+    public Text textPassword;
+    public Text textReenterPassword;
+       
+
+	// Use this for initialization
+	void Start () {
+        displayLoginPanel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void displayLoginPanel() {
+        buttonRegister.SetActive(false);
+        fieldReenterPassword.SetActive(false);
+    }
+}
