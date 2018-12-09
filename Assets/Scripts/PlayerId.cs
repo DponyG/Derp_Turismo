220eecffedf5b2ac4a421037ab614ab7a1c43efd
Samuel Grenon <dponyg@gmail.com>
2018-12-08 22:42:09 -0900

    Pushing new Resolution

---
+++ Assets/Scripts/PlayerId.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerId : MonoBehaviour {
+
+    public static PlayerId playerId;
+
+    private void Awake() {
+        if(playerId == null) {
+            DontDestroyOnLoad(gameObject);
+            playerId = this;
+        }
+        else if(playerId != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
