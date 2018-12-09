cf9c51087281c371360ae2e5379986449a0166b1
DuaneShaffer <dlshaffer@alaska.edu>
2018-12-08 18:38:59 -0900

    Add back buttons

--- Assets/Scripts/ModeSelection.cs
+++ Assets/Scripts/ModeSelection.cs
@@ -8,12 +8,14 @@ public class ModeSelection : MonoBehaviour {
 
 	public Button garageButton;
 	public Button raceButton;
+    public Button backButton;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		garageButton.onClick.AddListener(delegate {Navigate("GarageScene"); });
 		raceButton.onClick.AddListener(delegate {Navigate("RaceSelectionScene"); });
-	}
+        backButton.onClick.AddListener(delegate { Navigate("Menu"); });
+    }
 	
 	
 	void Navigate(string scene) {
---
+++ Assets/Scripts/Race_back.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Race_back : MonoBehaviour
+{
+
+    public Button backButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        backButton.onClick.AddListener(delegate { Navigate("SelectMode"); });
+    }
+
+
+    void Navigate(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
